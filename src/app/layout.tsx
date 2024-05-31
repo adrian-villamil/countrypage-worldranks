3b6f436e4fb427e53e81e68fa09ce9fedbe82d4f
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-05-31 18:33:54 -0500

    Root layout with a hero image and an icon in the background.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import type { Metadata } from "next";
 import { beVietnamPro } from "@/config/fonts";
 import "./globals.css";
@@ -14,7 +15,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={beVietnamPro.className}>{children}</body>
+      <body className={beVietnamPro.className}>
+        <div className="h-[300px] bg-black overflow-hidden absolute top-0 left-0 w-full -z-30">
+          <Image
+            src={'/hero-image-wr.jpg'}
+            alt="hero-image"
+            width={1280}
+            height={300}
+            priority
+            className="absolute top-11 xl:top-0 left-0 w-full h-[300px] object-cover -z-20"
+          />
+          <Image
+            src={'/Logo.svg'}
+            alt="Logo"
+            width={174}
+            height={24}
+            priority
+            className="absolute top-[120px] left-1/2 -translate-x-1/2 -z-10"
+          />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
