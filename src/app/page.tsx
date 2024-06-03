7f89934022a43a57f5396bd6825d00b62b646e02
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-03 18:18:41 -0500

    Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import { getAllCountries } from "@/actions/get-all-countries";
+import { getAllCountries } from "@/actions";
 import { Aside, Header, Table } from "@/components";
 
 export default async function HomePage() {
-  // const countries = await getAllCountries();
+  const countries = await getAllCountries();
   
   return (
     <main className="w-full lg:w-[984px] xl:w-[1200px] min-h-[600px] bg-dark-black shadow-xl border border-white/10 lg:rounded-xl mx-auto mt-60 px-[30px] pt-6">
       <Header />
       <div className="flex flex-col lg:flex-row gap-8">
         <Aside />
-        {/* <Table countries={countries} /> */}
+        <Table countries={countries} />
       </div>
     </main>
   );
