c97faaad7cbc7f1ef7d1ed897ce8f6fb1447d6f2
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-03 18:17:20 -0500

    Root Layout - added a hero image component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import type { Metadata } from "next";
 import { beVietnamPro } from "@/config/fonts";
 import "./globals.css";
+import { HeroImage } from "@/components";
 
 export const metadata: Metadata = {
   title: "Country Page - WorldRanks",
@@ -16,24 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={beVietnamPro.className}>
-        <div className="h-[300px] bg-black overflow-hidden absolute top-0 left-0 w-full -z-30">
-          <Image
-            src={'/hero-image-wr.jpg'}
-            alt="hero-image"
-            width={1280}
-            height={300}
-            priority
-            className="absolute top-11 xl:top-0 left-0 w-full h-[300px] object-cover -z-20"
-          />
-          <Image
-            src={'/Logo.svg'}
-            alt="Logo"
-            width={174}
-            height={24}
-            priority
-            className="absolute top-[120px] left-1/2 -translate-x-1/2 -z-10"
-          />
-        </div>
+        <HeroImage />
         {children}
       </body>
     </html>
