2ca703355114cf8b98e7b29a02f0441c0fb569a3
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-06 23:33:10 -0500

    Adding count value of how many countries are filtered.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { getAllCountries } from "@/actions";
 import { Aside, Header, Table } from "@/components";
+import type { SearchParams } from "@/interfaces";
 
-export default async function HomePage() {
-  const countries = await getAllCountries();
-  
+interface Props {
+  searchParams: SearchParams;
+}
+
+export default async function HomePage({ searchParams }: Props) {
+  const { count, countries } = await getAllCountries(searchParams);
+
   return (
     <main className="w-full lg:w-[984px] xl:w-[1200px] min-h-[600px] bg-dark-black shadow-xl border border-white/10 lg:rounded-xl mx-auto mt-60 px-[30px] pt-6">
-      <Header />
+      <Header count={count} />
       <div className="flex flex-col lg:flex-row gap-8">
         <Aside />
         <Table countries={countries} />
