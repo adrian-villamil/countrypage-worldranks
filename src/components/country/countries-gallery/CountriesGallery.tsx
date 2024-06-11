4a670930cd14b00cd45d31ac9c4fc635942f1f1a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-10 23:36:05 -0500

    Forgot to add key props countries gallery and skeleton's lists.

--- src/components/country/countries-gallery/CountriesGallery.tsx
+++ src/components/country/countries-gallery/CountriesGallery.tsx
@@ -12,7 +12,7 @@ export const CountriesGallery = async ({ borders }: Props) => {
   return (
     <div className="flex flex-wrap gap-4">
       {neighboringCountries.map((country) => country && (
-        <Link href={`/country/${country.cca3}`} className="flex flex-col gap-2">
+        <Link key={country.cca3} href={`/country/${country.cca3}`} className="flex flex-col gap-2">
           <Image
             src={country.flags.png}
             alt={country.name.common}
--- src/components/country/countries-gallery/CountriesGallerySkeleton.tsx
+++ src/components/country/countries-gallery/CountriesGallerySkeleton.tsx
@@ -2,7 +2,7 @@ export const CountriesGallerySkeleton = () => {
   return (
     <div className="animate-pulse flex flex-wrap gap-4">
       {Array.from({length: 5}).map((v,i) => (
-        <div className="flex flex-col gap-2">
+        <div key={i} className="flex flex-col gap-2">
           <div className="w-20 h-[60px] rounded bg-gray"></div>
           <div className="h-4 rounded-lg bg-gray"></div>
         </div>
