65a9df5eee45288de93061a5189ec24fa9278642
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-12 00:55:13 -0500

    You can now set the number of countries per page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 export default async function HomePage({ searchParams }: Props) {
-  const { count, totalPages, countries } = await getAllCountries(searchParams);
+  const { count, totalPages, countries } = await getAllCountries(searchParams, 10);
 
   return (
     <main className="w-full lg:w-[980px] xl:w-[1205px] min-h-[600px] bg-dark-black shadow-xl border border-white/10 lg:rounded-xl mx-auto mt-60 mb-10 px-[30px] py-6">
