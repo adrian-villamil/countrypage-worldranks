761431882dc04d67e38e0b366d95a9af91734f7d
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-12 00:48:33 -0500

    Restcountries api got fixed, so I don't need route handlers anymore.

--- src/app/api/country/[cca3]/route.ts
+++
@@ -1,9 +0,0 @@
-import { seedCountries } from '@/seed/seed-contries';
-import { NextResponse, NextRequest } from 'next/server'
-
-export async function GET(request: NextRequest, { params }: { params: { cca3: string } }) {
-  const { cca3 } = params;
-  const country = seedCountries.find((country) => country.cca3 === cca3);
-
-  return NextResponse.json(country, { status: 200 });
-}
